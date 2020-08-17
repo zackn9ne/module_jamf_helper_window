cb434374adf5aae024d162078e9eede9165da59d
Your Name <you@example.com>
2020-08-17 11:56:26 -0400

    setup edit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='jamf helper window tool',  
+     name='jamf_helper_window_tool',  
      version='0.1',
      scripts=['makewindow.py'] ,
      author="Zackn9ne",
