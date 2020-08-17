b93cb6852dc0eb652de87c96c05bcf578b32e642
Your Name <you@example.com>
2020-08-17 11:25:35 -0400

    with setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='jamf helper window tool',  
+     version='0.1',
+     scripts=['makewindow.py'] ,
+     author="Zackn9ne",
+     author_email="zackn9ne@gmail.com",
+     description="A jamf helper window creator tool",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/zackn9ne/module_jamf_helper_window.git",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
